fc64d80a8048a149f31eddda8c5e8b176d116020
Asel Hidallearachchi <92600731+AselInukeHidallearachchi@users.noreply.github.com>
2025-01-04 01:23:14 +0530

    w 1

---
+++ BMI version 1/BMI version 1/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  BMI version 1
+//
+//  Created by Asel Inuke Hidallearachchi on 2024-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: Declare variables to hold the height, weight, bmiValue and bmiMessage
+    // MARK: All variables must be initialized
+    @State private var height: String = ""
+    @State private var weight: String = ""
+    @State private var bmiValue: Double = 0.0
+    @State private var bmiMessage: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("BMI Calculator Ver1")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.all)
+            
+            TextField("Enter Height (m)", text: $height)
+                .keyboardType(.decimalPad)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 200, height: 50)
+            
+            TextField("Enter Weight (kg)", text: $weight)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 200, height: 50)
+                .padding()
+            
+            Button(action: calculateBMI) {
+                Text("Calculate BMI")
+            }
+            .padding()
+            
+            Text("BMI value: \(String(format: "%.2f", bmiValue))")
+            
+            Text(bmiMessage)
+                .font(.subheadline)
+                .padding()
+            
+            Spacer()
+        }
+    }
+    
+    func calculateBMI() {
+        
+        // Check that the height and weight entered are numeric and greater than 0, and cast them to Double
+        if let heightValue = Double(height), let weightValue = Double(weight), heightValue > 0, weightValue > 0 {
+            // Calculate BMI
+            let bmi = weightValue / (heightValue * heightValue)
+            bmiValue = bmi
+            
+            // Create a suitable message based on bmi value and assign to bmiMessage
+            if bmi < 18.5 {
+                bmiMessage = "Underweight"
+            } else if bmi < 24.9 {
+                bmiMessage = "Normal weight"
+            } else if bmi < 29.9 {
+                bmiMessage = "Overweight"
+            } else {
+                bmiMessage = "Obese"
+            }
+        } else {
+            // If either condition is false, show an error message
+            bmiValue = 0
+            bmiMessage = "Please enter valid values"
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ BMI version 1/BMI version 1Tests/BMI_version_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_version_1Tests.swift
+//  BMI version 1Tests
+//
+//  Created by Asel Inuke Hidallearachchi on 2024-11-05.
+//
+
+import Testing
+@testable import BMI_version_1
+
+struct BMI_version_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
