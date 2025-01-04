f1ecd428a4be71fff65a05bfef3a8b0d9479409d
Asel Hidallearachchi <92600731+AselInukeHidallearachchi@users.noreply.github.com>
2025-01-04 22:40:06 +0530

    w 1.1

---
+++ BMI version 2/BMI version 1/BMI_version_1App.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_version_1App.swift
+//  BMI version 1
+//
+//  Created by Asel Inuke Hidallearachchi on 2024-11-05.
+//
+
+import SwiftUI
+
+@main
+struct BMI_version_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
